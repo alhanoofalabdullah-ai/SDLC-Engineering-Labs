40af705eb0b432f73bcf8f0f2c88a0a2f508953e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-21 13:08:32 +0300

    Create AuditLog.java

---
+++ project-13-sdlc-audit-trail-system/AuditLog.java
@@ -0,0 +1,101 @@
+package com.alhanoof.sdlc.audit.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String moduleName;
+
+    private String actionType;
+
+    @Column(length = 3000)
+    private String actionDetails;
+
+    private String oldValue;
+
+    private String newValue;
+
+    private LocalDateTime actionDate;
+
+    private String ipAddress;
+
+    public AuditLog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(String actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getActionDetails() {
+        return actionDetails;
+    }
+
+    public void setActionDetails(String actionDetails) {
+        this.actionDetails = actionDetails;
+    }
+
+    public String getOldValue() {
+        return oldValue;
+    }
+
+    public void setOldValue(String oldValue) {
+        this.oldValue = oldValue;
+    }
+
+    public String getNewValue() {
+        return newValue;
+    }
+
+    public void setNewValue(String newValue) {
+        this.newValue = newValue;
+    }
+
+    public LocalDateTime getActionDate() {
+        return actionDate;
+    }
+
+    public void setActionDate(LocalDateTime actionDate) {
+        this.actionDate = actionDate;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+}
