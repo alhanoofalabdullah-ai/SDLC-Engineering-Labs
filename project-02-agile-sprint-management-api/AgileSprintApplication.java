525290fa93562ff4aa4224972e1035244312d6b7
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-21 09:15:03 +0300

    Create AgileSprintApplication.java

---
+++ project-02-agile-sprint-management-api/AgileSprintApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.sdlc.agile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileSprintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgileSprintApplication.class, args);
+    }
+}
