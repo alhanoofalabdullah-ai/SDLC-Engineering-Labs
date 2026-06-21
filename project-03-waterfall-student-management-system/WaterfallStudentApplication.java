6aeeed6505333716ba98af8748a4151de06c036a
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-21 10:07:22 +0300

    Create WaterfallStudentApplication.java

---
+++ project-03-waterfall-student-management-system/WaterfallStudentApplication.java
@@ -0,0 +1,12 @@
+package com.alhanoof.sdlc.waterfall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterfallStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterfallStudentApplication.class, args);
+    }
+}
